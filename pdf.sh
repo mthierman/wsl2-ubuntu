a2809f683943fa5adc655be95332627e8d3b197f
mthierman <mthierman@gmail.com>
2025-09-01 13:58:05 -0600

    Update pdf with Ubuntu 22.04 (Jammy)

--- pdf.sh
+++ pdf.sh
@@ -1,37 +1,75 @@
+# https://source.chromium.org/chromium/chromium/src/+/main:chrome/installer/linux/debian/dist_package_versions.json;bpv=0
+# "Ubuntu 22.04 (Jammy)": {
+#     "libasound2": "1.2.6.1-1ubuntu1",
+#     "libatk-bridge2.0-0": "2.38.0-3",
+#     "libatk1.0-0": "2.36.0-3build1",
+#     "libatspi2.0-0": "2.44.0-3",
+#     "libc6": "2.35-0ubuntu3.8",
+#     "libcairo2": "1.16.0-5ubuntu2",
+#     "libcups2": "2.4.1op1-1ubuntu4.10",
+#     "libdbus-1-3": "1.12.20-2ubuntu4.1",
+#     "libdrm2": "2.4.113-2~ubuntu0.22.04.1",
+#     "libexpat1": "2.4.7-1ubuntu0.3",
+#     "libgbm1": "23.2.1-1ubuntu3.1~22.04.2",
+#     "libglib2.0-0": "2.72.4-0ubuntu2.3",
+#     "libnspr4": "2:4.35-0ubuntu0.22.04.1",
+#     "libnss3": "2:3.98-0ubuntu0.22.04.2",
+#     "libpango-1.0-0": "1.50.6+ds-2ubuntu1",
+#     "libpangocairo-1.0-0": "1.50.6+ds-2ubuntu1",
+#     "libstdc++6": "12.3.0-1ubuntu1~22.04",
+#     "libudev1": "249.11-0ubuntu3.7",
+#     "libuuid1": "2.37.2-4ubuntu3.4",
+#     "libx11-6": "2:1.7.5-1ubuntu0.3",
+#     "libx11-xcb1": "2:1.7.5-1ubuntu0.3",
+#     "libxcb-dri3-0": "1.14-3ubuntu3",
+#     "libxcb1": "1.14-3ubuntu3",
+#     "libxcomposite1": "1:0.4.5-1build2",
+#     "libxcursor1": "1:1.2.0-2build4",
+#     "libxdamage1": "1:1.1.5-2build2",
+#     "libxext6": "2:1.3.4-1build1",
+#     "libxfixes3": "1:6.0.0-1",
+#     "libxi6": "2:1.8-1build1",
+#     "libxkbcommon0": "1.4.0-1",
+#     "libxrandr2": "2:1.5.2-1build1",
+#     "libxrender1": "1:0.9.10-1build4",
+#     "libxshmfence1": "1.3-1build4",
+#     "libxss1": "1:1.2.3-1build2",
+#     "libxtst6": "2:1.2.3-1build4"
+# }
+sudo apt update
 sudo apt install -y \
-ca-certificates \
-fonts-liberation \
-# libasound2 \
-# libatk-bridge2.0-0 \
-# libatk1.0-0 \
-# libc6 \
-libcairo2 \
-libcups2 \
-libdbus-1-3 \
-libexpat1 \
-libfontconfig1 \
-libgbm1 \
-libgcc1 \
-libglib2.0-0 \
-libgtk-3-0 \
-libnspr4 \
-libnss3 \
-libpango-1.0-0 \
-libpangocairo-1.0-0 \
-libstdc++6 \
-libx11-6 \
-libx11-xcb1 \
-libxcb1 \
-libxcomposite1 \
-libxcursor1 \
-libxdamage1 \
-libxext6 \
-libxfixes3 \
-libxi6 \
-libxrandr2 \
-libxrender1 \
-libxss1 \
-libxtst6 \
-lsb-release \
-wget \
-xdg-utils \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libatspi2.0-0 \
+    libc6 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libdrm2 \
+    libexpat1 \
+    libgbm1 \
+    libglib2.0-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libstdc++6 \
+    libudev1 \
+    libuuid1 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb-dri3-0 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxkbcommon0 \
+    libxrandr2 \
+    libxrender1 \
+    libxshmfence1 \
+    libxss1 \
+    libxtst6
