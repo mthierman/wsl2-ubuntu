d29b26814df30cd06a3150034d960e175d717e82
mthierman <mthierman@gmail.com>
2026-04-28 11:21:45 -0600

    Update pwsh script

--- pwsh.sh
+++ pwsh.sh
@@ -1,5 +1,5 @@
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.5.2/powershell_7.5.2-1.deb_amd64.deb
-sudo dpkg -i powershell_7.5.2-1.deb_amd64.deb
-rm powershell_7.5.2-1.deb_amd64.deb
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.6.1/powershell_7.6.1-1.deb_amd64.deb
+sudo dpkg -i powershell_7.6.1-1.deb_amd64.deb
+rm powershell_7.6.1-1.deb_amd64.deb
 sudo apt update
 sudo apt install -y powershell
